9f117a3ae042a7802200c805e4c7414e45945e15
Marty <marsmith@usgs.gov>
2015-04-30 13:28:31 -0400

    update

--- server-config/PythonAppServers.sh
+++ server-config/PythonAppServers.sh
@@ -1,5 +1,4 @@
 /usr/bin/nohup python /var/www/mapper/exporter/export.py > /dev/null 2>&1 &
 /usr/bin/nohup python /var/www/mapper/exporter/exportSM.py > /dev/null 2>&1 &
 /usr/bin/nohup python /var/www/mapper/exporter/siteCounter.py  > /dev/null 2>&1 &
-/usr/bin/nohup python /var/www/mapper/exporter/fileUpload.py  > /dev/null 2>&1 &
 /usr/bin/nohup python /var/www/mapper/exporter/aquifers.py  > /dev/null 2>&1 &

88ad90a7b359966a3c1095f51be52cd7ed2a69a6
Marty <marsmith@usgs.gov>
2015-05-01 10:53:24 -0400

    update

--- server-config/server-setup.sh
+++ server-config/server-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 #list of apps
 LIST_OF_MAIN_APPS="python python-pip git libgeos-dev apache2"
@@ -15,33 +16,27 @@ a2enmod proxy_http
 #get website content from github
 git clone https://github.com/USGS-OWI/nwis-mapper.git
 mv nwis-mapper mapper
-ln -s /home/osboxes/mapper /var/www 
+ln -s ${USER_HOME}/mapper /var/www 
+
 
 #start up cherrypy services
-sh /home/osboxes/mapper/server-config/PythonAppServers.sh
+sh ${USER_HOME}/mapper/server-config/PythonAppServers.sh
 
-#add proxy config to default virtualhost site
-if grep -q "apache mod_proxy setup" "/etc/apache2/sites-available/000-default.conf"; then
-	echo "Already appended mod-proxy setup"
-else
-	echo "#apache mod_proxy setup" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/nwis/site http://waterservices.usgs.gov/nwis/site" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/nwis/site http://waterservices.usgs.gov/nwis/site" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/sitesmapper http://waterdata.usgs.gov" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/sitesmapper http://waterdata.usgs.gov" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/nwissitesmapper http://nwis.waterdata.usgs.gov" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/nwissitesmapper http://nwis.waterdata.usgs.gov" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/wamapper http://waterservices.usgs.gov/nwis/iv" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/wamapper http://waterservices.usgs.gov/nwis/iv" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/export http://localhost:8080/exportFile" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/export http://localhost:8080/exportFile" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/exportSM http://localhost:8081/exportFileSM" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/exportSM http://localhost:8081/exportFileSM" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/sitecounter http://localhost:9998/counterProcess" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/sitecounter http://localhost:9998/counterProcess" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPass /mapper/tileRGB http://localhost:8085/tileRGBValue" >> /etc/apache2/sites-available/000-default.conf
-	echo "ProxyPassReverse /mapper/tileRGB http://localhost:8085/tileRGBValue" >> /etc/apache2/sites-available/000-default.conf
+#setup up crons
+crontab -l | { cat; echo "*/5 * * * * ${USER_HOME}/mapper/server-config/chkCherry.sh"; } | crontab -
+crontab -l | { cat; echo "0 0 * * 0 rm -rf ${USER_HOME}/mapper/exporter/temp/*"; } | crontab -
 
-#restart apache2
+#add redirect for /
+cp ${USER_HOME}/mapper/server-config/index.html /var/www/index.html
+
+#remove default html folder
+rm -R html
+
+#add new virtual site
+cp ${USER_HOME}/mapper/server-config/nwis-mapper.conf /etc/apache2/sites-available/nwis-mapper.conf
+a2dissite 000-default
+a2ensite nwis-mapper
 service apache2 restart
+
+#end
 fi
